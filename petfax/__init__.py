45914c2e83f9af4f3dfbed2908b5fa0072896048
Caroline Konrad <ckonrad@live.unc.edu>
2022-09-05 20:54:19 -0400

    Displaying facts from database

--- petfax/__init__.py
+++ petfax/__init__.py
@@ -1,7 +1,15 @@
 from flask import Flask
+from flask_migrate import Migrate
 
 def create_app():
     app = Flask(__name__)
+
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Tannerrocks1@localhost:3000/petfax'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False             
+
+    from . import models
+    models.db.init_app(app)
+    migrate = Migrate(app, models.db)
 
     @app.route('/')
     def hello():
@@ -14,3 +22,4 @@ def create_app():
     app.register_blueprint(fact.bp)
     
     return app
+         
